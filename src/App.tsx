1ff3f11a93743d251d423fe18208f67c8b811dcf
coprocoder <mog.ie@mail.ru>
2023-02-24 08:44:47 +0700

    fix: changed router from hash to browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Routes, Route, HashRouter} from "react-router-dom";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 import loadable from "@loadable/component";
 
 import Waiter from "./components/waiter";
@@ -17,13 +17,13 @@ const QuotesPage = loadable(() => import("./pages/quotes"), {
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<AboutPage />} />
         <Route path="/quotes" element={<QuotesPage />} />
         <Route path="/quotes/:id" element={<QuotesPage />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
